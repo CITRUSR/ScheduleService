2932f76e447b1e1c5ef2183e180ddc6e5627462c
CITRUSR <savelypetrov2007@mail.ru>
2024-10-30 17:54:57 +0300

    add table names

---
+++ ScheduleService.Infrastructure/Migrations/TableNames.cs
@@ -0,0 +1,14 @@
+namespace ScheduleService.Infrastructure.Migrations;
+
+public static class TableNames
+{
+    public const string SUBJECTS = "subjects";
+    public const string SPECIAL_SUBJECTS = "special_subjects";
+    public const string TEACHERS_SUBJECTS_GROUPS = "teachers_subjects_groups";
+    public const string TEACHERS_SUBJECTS = "teachers_subjects";
+    public const string SCHEDULES = "schedules";
+    public const string ROOMS = "rooms";
+    public const string WEEKDAYS = "weekdays";
+    public const string ROOMS_SCHEDULES = "rooms_schedules";
+    public const string SCHEDULES_TEACHERS_SUBJECTS = "schedules_teachers_subjects";
+}
