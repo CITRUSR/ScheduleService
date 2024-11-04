b21515a39875b445d0a7e00e000b1585f290bb77
CITRUSR <savelypetrov2007@mail.ru>
2024-11-05 01:22:02 +0300

    remove ensure create database functional

--- ScheduleService.Infrastructure/DbContext.cs
+++ ScheduleService.Infrastructure/DbContext.cs
@@ -13,29 +13,10 @@ public class DbContext : IDbContext
     public DbContext(IConfiguration configuration)
     {
         _configuration = configuration;
-        EnsureCreated("scheduleDb");
     }
 
     public IDbConnection CreateConnection()
     {
         return new NpgsqlConnection(_configuration.GetConnectionString("DbConnectionString"));
-    }
-
-    public void EnsureCreated(string name)
-    {
-        var parameters = new DynamicParameters();
-
-        parameters.Add("name", name);
-
-        using var connection = CreateConnection();
-
-        var databaseExists = connection.QueryFirstOrDefault<bool>(
-            "SELECT EXISTS (SELECT 1 FROM pg_database WHERE datname = @name)"
-        );
-
-        if (!databaseExists)
-        {
-            connection.Execute("CREATE DATABASE @name", new { name });
-        }
     }
 }
