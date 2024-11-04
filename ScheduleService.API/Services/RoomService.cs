78ef7367b84d11efae2b4542374b69c1a1f67217
CITRUSR <savelypetrov2007@mail.ru>
2024-11-05 02:10:44 +0300

    add create room to grpc service

--- ScheduleService.API/Services/RoomService.cs
+++ ScheduleService.API/Services/RoomService.cs
@@ -1,8 +1,23 @@
+using Grpc.Core;
+using Mapster;
 using MediatR;
+using ScheduleService.Application.CQRS.RoomEntity.Commands.CreateRoom;
 
 namespace ScheduleService.API.Services;
 
 public class RoomService(IMediator mediator) : ScheduleService.RoomService.RoomServiceBase
 {
     private readonly IMediator _mediator = mediator;
+
+    public override async Task<CreateRoomResponse> CreateRoom(
+        CreateRoomRequest request,
+        ServerCallContext context
+    )
+    {
+        var command = request.Adapt<CreateRoomCommand>();
+
+        var room = await _mediator.Send(command);
+
+        return new CreateRoomResponse { Room = room.Adapt<Room>() };
+    }
 }
