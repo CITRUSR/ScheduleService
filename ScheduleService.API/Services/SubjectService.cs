d1cef767bf85d238a2c4e4f803934921d9bc6323
CITRUSR <savelypetrov2007@mail.ru>
2024-11-11 00:18:44 +0300

    add create subject to grpc service

--- ScheduleService.API/Services/SubjectService.cs
+++ ScheduleService.API/Services/SubjectService.cs
@@ -1,8 +1,23 @@
+using Grpc.Core;
+using Mapster;
 using MediatR;
+using ScheduleService.Application.CQRS.SubjectEntity.Commands.CreateSubject;
 
 namespace ScheduleService.API.Services;
 
 public class SubjectService(IMediator mediator) : ScheduleService.SubjectService.SubjectServiceBase
 {
     private readonly IMediator _mediator = mediator;
+
+    public override async Task<CreateSubjectResponse> CreateSubject(
+        CreateSubjectRequest request,
+        ServerCallContext context
+    )
+    {
+        var command = request.Adapt<CreateSubjectCommand>();
+
+        var subject = await _mediator.Send(command);
+
+        return new CreateSubjectResponse() { Subject = subject.Adapt<Subject>() };
+    }
 }
