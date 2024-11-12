0fe2e65a38c895aa1864794266fe2cb0f848a9ac
CITRUSR <savelypetrov2007@mail.ru>
2024-11-13 02:08:21 +0300

    add get weekday by id to grpc service

--- ScheduleService.API/Services/WeekdayService.cs
+++ ScheduleService.API/Services/WeekdayService.cs
@@ -1,8 +1,23 @@
+using Grpc.Core;
+using Mapster;
 using MediatR;
+using ScheduleService.Application.CQRS.WeekdayEntity.Queries.GetWeekdayById;
 
 namespace ScheduleService.API.Services;
 
 public class WeekdayService(IMediator mediator) : ScheduleService.WeekdayService.WeekdayServiceBase
 {
     private readonly IMediator _mediator = mediator;
+
+    public override async Task<GetWeekdayByIdResponse> GetWeekdayById(
+        GetWeekdayByIdRequest request,
+        ServerCallContext context
+    )
+    {
+        var query = request.Adapt<GetWeekdayByIdQuery>();
+
+        var weekday = await _mediator.Send(query);
+
+        return new GetWeekdayByIdResponse { Weekday = weekday.Adapt<Weekday>() };
+    }
 }
