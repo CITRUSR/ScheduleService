03ce968e14a4c540bb78496f7775c279ecfdbdc2
CITRUSR <savelypetrov2007@mail.ru>
2024-11-14 00:31:17 +0300

    refactorign seperate program to extension methods

---
+++ ScheduleService.API/extensions/SturtupExtension.cs
@@ -0,0 +1,43 @@
+using ScheduleService.API.Interceptors;
+using ScheduleService.Application;
+using ScheduleService.Infrastructure;
+
+namespace ScheduleService.API.extensions;
+
+public static class SturtupExtension
+{
+    public static void ConfigureServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddGrpc(options =>
+        {
+            options.Interceptors.Add<ServerExceptionsInterceptor>();
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddApplication();
+        services.AddInfrastructure(configuration);
+
+        services.AddGrpc();
+    }
+
+    public static void ConfigureApplication(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapGrpcService<Services.ColorService>();
+        app.MapGrpcService<Services.RoomService>();
+        app.MapGrpcService<Services.SubjectService>();
+        app.MapGrpcService<Services.WeekdayService>();
+    }
+}
