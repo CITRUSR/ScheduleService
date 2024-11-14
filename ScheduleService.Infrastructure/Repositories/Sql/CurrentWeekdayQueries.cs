f21425d0cfddafb35b24c8bb6d7968312453a99c
CITRUSR <savelypetrov2007@mail.ru>
2024-11-14 23:45:04 +0300

    add get current weekday to repository

--- ScheduleService.Infrastructure/Repositories/Sql/CurrentWeekdayQueries.cs
+++ ScheduleService.Infrastructure/Repositories/Sql/CurrentWeekdayQueries.cs
@@ -1,3 +1,10 @@
 namespace ScheduleService.Infrastructure.Repositories.Sql;
 
-public static class CurrentWeekdayQueries { }
+public static class CurrentWeekdayQueries
+{
+    public static string GetCurrentWeekday =
+        @"
+            SELECT id, color, interval, updated_at AS UpdatedAt FROM current_weekday
+            LIMIT 1;
+        ";
+}
