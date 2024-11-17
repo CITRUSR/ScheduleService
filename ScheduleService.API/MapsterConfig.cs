50d5e7ee7ff42f2a4a9309b859e96a5224e07473
CITRUSR <savelypetrov2007@mail.ru>
2024-11-17 21:52:35 +0300

    add mapster configuration for create current weekday

--- ScheduleService.API/MapsterConfig.cs
+++ ScheduleService.API/MapsterConfig.cs
@@ -1,6 +1,7 @@
 using Google.Protobuf.WellKnownTypes;
 using Mapster;
 using ScheduleService.API.extensions;
+using ScheduleService.Application.CQRS.CurrentWeekdayEntity.Commands.CreateCurrentWeekday;
 using ScheduleService.Application.CQRS.CurrentWeekdayEntity.Commands.UpdateCurrentWeekday;
 
 namespace ScheduleService.API;
@@ -10,6 +11,10 @@ public static class MapsterConfig
     public static void Configure()
     {
         TypeAdapterConfig<UpdateCurrentWeekdayRequest, UpdateCurrentWeekdayCommand>
+            .NewConfig()
+            .Map(dest => dest.Interval, src => src.Interval.ToTimeSpan())
+            .Map(dest => dest.UpdateTime, src => src.UpdateTime.ToDateTime());
+        TypeAdapterConfig<CreateCurrentWeekdayRequest, CreateCurrentWeekdayCommand>
             .NewConfig()
             .Map(dest => dest.Interval, src => src.Interval.ToTimeSpan())
             .Map(dest => dest.UpdateTime, src => src.UpdateTime.ToDateTime());
