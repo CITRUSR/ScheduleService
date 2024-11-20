cc4ab86b4c14da277ad5e1b03047da26af6d1292
CITRUSR <savelypetrov2007@mail.ru>
2024-11-21 00:47:28 +0300

    add current weekday update task creator

--- ScheduleService.Application/DependencyInjection.cs
+++ ScheduleService.Application/DependencyInjection.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using ScheduleService.Application.Common.Behaviours;
+using ScheduleService.Application.Contracts;
+using ScheduleService.Application.Services;
 
 namespace ScheduleService.Application;
 
@@ -17,6 +19,8 @@ public static class DependencyInjection
 
             cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
         });
+
+        services.AddScoped<ICurrentWeekdayUpdateTaskCreator, CurrentWeekdayUpdateTaskCreator>();
 
         return services;
     }
