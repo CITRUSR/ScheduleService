54bb9cbc6269714895559161c491f7ac4bcb75b2
CITRUSR <savelypetrov2007@mail.ru>
2024-11-27 00:58:46 +0300

    refactoring mapster config. Split into some methods
    sq before refactoring mapster config

--- ScheduleService.API/MapsterConfig.cs
+++ ScheduleService.API/MapsterConfig.cs
@@ -1,6 +1,6 @@
+using Google.Protobuf.Collections;
 using Google.Protobuf.WellKnownTypes;
 using Mapster;
-using ScheduleService.API.extensions;
 using ScheduleService.Application.CQRS.CurrentWeekdayEntity.Commands.CreateCurrentWeekday;
 using ScheduleService.Application.CQRS.CurrentWeekdayEntity.Commands.UpdateCurrentWeekday;
 
@@ -10,14 +10,31 @@ public static class MapsterConfig
 {
     public static void Configure()
     {
+        TypeAdapterConfig.GlobalSettings.Default.UseDestinationValue(member =>
+            member.SetterModifier == AccessModifier.None
+            && member.Type.IsGenericType
+            && member.Type.GetGenericTypeDefinition() == typeof(RepeatedField<>)
+        );
+
+        ConfigureCurrentWeekdayRequests();
+        ConfigureCurrentWeekdayEntity();
+    }
+
+    private static void ConfigureCurrentWeekdayRequests()
+    {
         TypeAdapterConfig<UpdateCurrentWeekdayRequest, UpdateCurrentWeekdayCommand>
             .NewConfig()
             .Map(dest => dest.Interval, src => src.Interval.ToTimeSpan())
             .Map(dest => dest.UpdateTime, src => src.UpdateTime.ToDateTime());
+
         TypeAdapterConfig<CreateCurrentWeekdayRequest, CreateCurrentWeekdayCommand>
             .NewConfig()
             .Map(dest => dest.Interval, src => src.Interval.ToTimeSpan())
             .Map(dest => dest.UpdateTime, src => src.UpdateTime.ToDateTime());
+    }
+
+    private static void ConfigureCurrentWeekdayEntity()
+    {
         TypeAdapterConfig<Domain.Entities.CurrentWeekday, CurrentWeekday>
             .NewConfig()
             .Map(dest => dest.Interval, src => src.Interval.ToDuration());
