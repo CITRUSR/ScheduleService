601c420fe6c48d4c717284f683a96f0257d98c86
CITRUSR <savelypetrov2007@mail.ru>
2025-01-05 02:09:14 +0300

    add group and teacher view models

---
+++ ScheduleService.Application/Contracts/UserService/Group/Dto/Responses/GroupViewModel.cs
@@ -0,0 +1,7 @@
+namespace ScheduleService.Application.Contracts.UserService.Group.Dto.Responses;
+
+public class GroupViewModel
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+}
