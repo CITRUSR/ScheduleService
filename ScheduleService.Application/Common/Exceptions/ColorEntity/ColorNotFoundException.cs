9e32d5586aae938e7df5fc334020be9bc2b9bc23
CITRUSR <savelypetrov2007@mail.ru>
2025-01-09 01:34:40 +0300

    add overload for color not found exception with name

--- ScheduleService.Application/Common/Exceptions/ColorEntity/ColorNotFoundException.cs
+++ ScheduleService.Application/Common/Exceptions/ColorEntity/ColorNotFoundException.cs
@@ -4,4 +4,7 @@ public class ColorNotFoundException : NotFoundException
 {
     public ColorNotFoundException(int id)
         : base($"color with id {id} not found") { }
+
+    public ColorNotFoundException(string name)
+        : base($"color with name '{name}' not found") { }
 }
