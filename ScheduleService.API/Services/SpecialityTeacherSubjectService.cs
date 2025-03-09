c8a8d40654ed0ea96307c2b18274182ae9db0bbe
CITRUSR <savelypetrov2007@mail.ru>
2025-03-09 23:42:41 +0300

    add get all speciality teacher subjects endpoint

--- ScheduleService.API/Services/SpecialityTeacherSubjectService.cs
+++ ScheduleService.API/Services/SpecialityTeacherSubjectService.cs
@@ -1,4 +1,8 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Mapster;
 using MediatR;
+using ScheduleService.Application.CQRS.SpecialityTeacherSubjectEntity.Queries.GetAllSpecialityTeacherSubjects;
 
 namespace ScheduleService.API.Services;
 
@@ -6,4 +10,17 @@ public class SpecialityTeacherSubjectService(IMediator mediator)
     : ScheduleService.SpecialityTeacherSubjectService.SpecialityTeacherSubjectServiceBase
 {
     private readonly IMediator _mediator = mediator;
+
+    public override async Task<GetSpecialityTeacherSubjectsResponse> GetAllSpecialityTeacherSubjects(
+        Empty request,
+        ServerCallContext context
+    )
+    {
+        var entities = await _mediator.Send(new GetAllSpecialityTeacherSubjectsQuery());
+
+        return new GetSpecialityTeacherSubjectsResponse
+        {
+            SpecialityTeacherSubjects = { entities.Adapt<List<SpecialityTeacherSubjectModel>>() },
+        };
+    }
 }
