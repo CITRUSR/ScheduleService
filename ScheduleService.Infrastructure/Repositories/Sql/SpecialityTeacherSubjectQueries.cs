a001ee70f1a9dcd05a2fd2f45759e119ffc5dd4e
CITRUSR <savelypetrov2007@mail.ru>
2025-03-09 23:38:05 +0300

    add get all speciality teacher subjects to repository

--- ScheduleService.Infrastructure/Repositories/Sql/SpecialityTeacherSubjectQueries.cs
+++ ScheduleService.Infrastructure/Repositories/Sql/SpecialityTeacherSubjectQueries.cs
@@ -1,3 +1,10 @@
 namespace ScheduleService.Infrastructure.Repositories.Sql;
 
-public static class SpecialityTeacherSubjectQueries { }
+public static class SpecialityTeacherSubjectQueries
+{
+    public static readonly string GetAll =
+        @$"
+        SELECT speciality_fk AS SpecialityId, course, subgroup, teacher_fk AS TeacherId, subject_fk AS SubjectId
+        FROM specialities_teachers_subjects
+    ";
+}
