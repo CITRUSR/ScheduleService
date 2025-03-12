e117b449028cc49fb80c4a6749f0830152994388
CITRUSR <savelypetrov2007@mail.ru>
2025-03-13 01:01:06 +0300

    refactoring queries

--- ScheduleService.Infrastructure/Repositories/Sql/SubjectQueries.cs
+++ ScheduleService.Infrastructure/Repositories/Sql/SubjectQueries.cs
@@ -2,31 +2,31 @@ namespace ScheduleService.Infrastructure.Repositories.Sql;
 
 public static class SubjectQueries
 {
-    public static string InsertSubject =
+    public static readonly string InsertSubject =
         @"
             INSERT INTO subjects (name, abbreviation)
             VALUES (@Name, @Abbreviation)
             RETURNING id;
         ";
-    public static string GetSubjectById =
+    public static readonly string GetSubjectById =
         @"
             SELECT * FROM subjects
             WHERE id = @SubjectId;
         ";
-    public static string UpdateSubject =
+    public static readonly string UpdateSubject =
         @"
             UPDATE subjects
             SET name = @Name,
             abbreviation = @Abbreviation
             WHERE id = @Id;
         ";
-    public static string DeleteSubject =
+    public static readonly string DeleteSubject =
         @"
             DELETE FROM subjects
             WHERE id = @Id
             RETURNING *;
         ";
-    public static string GetSubjects =
+    public static readonly string GetSubjects =
         @"
             SELECT * FROM subjects
         ";
