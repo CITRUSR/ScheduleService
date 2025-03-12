fb1b1fb86ed6a7e427d571622d23e60d45d34229
CITRUSR <savelypetrov2007@mail.ru>
2025-03-12 22:21:20 +0300

    refactoring color handler to use unique checker

--- ScheduleService.Application/CQRS/ColorEntity/Commands/CreateColor/CreateColorCommandHandler.cs
+++ ScheduleService.Application/CQRS/ColorEntity/Commands/CreateColor/CreateColorCommandHandler.cs
@@ -1,15 +1,18 @@
 using MediatR;
-using Npgsql;
 using ScheduleService.Application.Common.Exceptions;
 using ScheduleService.Application.Contracts;
+using ScheduleService.Application.Contracts.Helpers;
 using ScheduleService.Domain.Entities;
 
 namespace ScheduleService.Application.CQRS.ColorEntity.Commands.CreateColor;
 
-public class CreateColorCommandHandler(IUnitOfWork unitOfWork)
-    : IRequestHandler<CreateColorCommand, Color>
+public class CreateColorCommandHandler(
+    IUnitOfWork unitOfWork,
+    IUniqueConstraintExceptionChecker uniqueChecker
+) : IRequestHandler<CreateColorCommand, Color>
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IUniqueConstraintExceptionChecker _uniqueChecker = uniqueChecker;
 
     public async Task<Color> Handle(CreateColorCommand request, CancellationToken cancellationToken)
     {
@@ -23,14 +26,15 @@ public class CreateColorCommandHandler(IUnitOfWork unitOfWork)
 
             _unitOfWork.CommitTransaction();
         }
-        catch (NpgsqlException ex) when (ex.SqlState == "23505")
+        catch (Exception ex)
         {
-            _unitOfWork.RollbackTransaction();
+            var propName = _uniqueChecker.Check<Color>(ex);
 
-            throw new ColorNameAlreadyExistsException(request.Name);
-        }
-        catch
-        {
+            if (propName != null && propName.Equals("name", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ColorNameAlreadyExistsException(request.Name);
+            }
+
             _unitOfWork.RollbackTransaction();
             throw;
         }
